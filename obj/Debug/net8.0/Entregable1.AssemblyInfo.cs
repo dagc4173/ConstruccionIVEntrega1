fa41c05193ab8966264bf21bb058ae4428722a9d
dagc4173 <diego.gutierrez50@correo.tedea.edu.co>
2023-08-27 06:06:52 -0500

    primer commit

--- obj/Debug/net8.0/Entregable1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Entregable1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entregable1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b2955ce95ef18d469f0b89f32aad1bcd72fb46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entregable1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entregable1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
